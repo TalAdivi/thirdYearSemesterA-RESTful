7738b67a27c91f36d3955d0d01684cb3f253b904
TalAdivi <taladivi10@gmail.com>
2019-12-08 15:29:58 +0200

    test

--- Tirgol/tirgolMongoDB/router.js
+++ Tirgol/tirgolMongoDB/router.js
@@ -15,14 +15,14 @@ restaurantRouter.get('/all', restaurantController.get);
 restaurantRouter.get('/:id', restaurantController.get);
 restaurantRouter.post('/', restaurantController.post);
 
-userRouter.get('/:id', userController.get);
-userRouter.post('/', userController.post);
-userRouter.put('/:id', userController.put);
+// userRouter.get('/:id', userController.get);
+// userRouter.post('/', userController.post);
+// userRouter.put('/:id', userController.put);
 
-orderRouter.get('/:id', orderController.get);
-orderRouter.post('/', orderController.post);
-orderRouter.put('/:id', orderController.put);
-orderRouter.delete('/:id', orderController.delete);
+// orderRouter.get('/:id', orderController.get);
+// orderRouter.post('/', orderController.post);
+// orderRouter.put('/:id', orderController.put);
+// orderRouter.delete('/:id', orderController.delete);
 
 module.exports = {
     restaurantRouter,
